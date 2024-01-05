bc302980837f3fd0ff7bf5be22f04174b748d8a6
Joseph Massa <jmassa@Josephs-MacBook-Pro.local>
2024-01-05 15:35:19 -0500

    Allow for test case input through a text file

--- solver.py
+++ solver.py
@@ -1,6 +1,33 @@
 import sys
 
+# fill board with 0's or fill from input file
+def fill_board(filename):
+    if not filename:
+        return [[0] * 9 for _ in range(9)]
 
+    board = []
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+        # check num of rows in board
+        if len(lines) != 9:
+            return None
+
+        for line in lines:
+            row = []
+            for num in line:
+                if num != '\n':
+                    row.append(int(num))
+            
+            # check each row length
+            if len(row) != 9:
+                return None 
+            
+            board.append(row)
+    
+    return board
+
+# print the current contents of the board
 def print_board(board):
     for i in range(len(board)):
         if i % 3 == 0 and i != 0:
@@ -9,11 +36,11 @@ def print_board(board):
         for j in range(len(board[0])):
             if j % 3 == 0 and j != 0:
                 print("| ", end="")
-            
 
             print(str(board[i][j]) + " ", end="")
         print("")
 
+# check if the value is already in this 3x3 box
 def isUsedInBox(board, boxRow, boxCol, val):
     for r in range(3):
         for c in range(3):
@@ -21,24 +48,30 @@ def isUsedInBox(board, boxRow, boxCol, val):
                 return True
     return False
 
+# check if the value is already in this row
 def isUsedInRow(board, row, val):
     for col in range(len(board[0])):
         if board[row][col] == val:
             return True
     return False
 
+# check if the value is already in this column
 def isUsedInCol(board, col, val):
     for row in range(len(board)):
         if board[row][col] == val:
             return True
     return False
 
+# check if this place is value is valid in this position
+# position needs to be equal to 0 (not already filled)
+# position needs to not have this value in this row, column, or 3x3 box
 def isValid(board, row, col, val):
     return board[row][col] == 0 and \
         not isUsedInRow(board, row, val) and \
         not isUsedInCol(board, col, val) and \
         not isUsedInBox(board, row - row%3, col - col%3, val)
 
+# find the next empty position in the board
 def findEmpty(board):
     for r in range(len(board)):
         for c in range(len(board)):
@@ -47,6 +80,10 @@ def findEmpty(board):
             
     return None
 
+# recursively solve the sudoku board using backtracking
+# if we can find another empty square, loop through values 1 through 9
+# for each value, check if we can place it at the empty position
+# if we can recurse to solve, if solve returns false then we are backtracking and need to try a different value at this position
 def solve(board):
     find = findEmpty(board)
     if not find:
@@ -58,24 +95,30 @@ def solve(board):
         if isValid(board, row, col, val):
             board[row][col] = val
 
-        if solve(board):
-            return True
-        
-        board[row][col] = 0
+            if solve(board):
+                return True
+            
+            board[row][col] = 0
 
     return False
 
+def main(filename):
+    board = fill_board(filename)
 
+    if not board:
+        print("Error: Invalid board")
+        exit(1)
 
-
-def main(args):
-    board = [[0] * 9 for _ in range(9)]
+    print("\nStarting board:")
+    print_board(board)
+    
     if solve(board):
+        print("\nSolution:")
         print_board(board)
     else:
-        print("There is no solution for this board.")
-
+        print("There is no solution for this board.\n")
 
 if __name__ == "__main__":
     args = sys.argv
-    main(args)
+    filename = args[1] if len(args) > 1 else None
+    main(filename)
